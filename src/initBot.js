52aae3ba595c6130c00dff0d62dd2911bd08dc23
reatexpk <reatexpk1@gmail.com>
2019-06-26 17:30:20 +0500

    fix: disable proxy by default

--- src/initBot.js
+++ src/initBot.js
@@ -2,17 +2,22 @@ require('dotenv').config();
 const SocksAgent = require('socks5-https-client/lib/Agent');
 const Telegraf = require('telegraf');
 
-function initBot() {
-  const socksAgent = new SocksAgent({
-    socksHost: process.env.PROXY_HOST,
-    socksPort: process.env.PROXY_PORT,
-    socksUsername: process.env.PROXY_USER,
-    socksPassword: process.env.PROXY_PASSWORD,
-  });
+function initBot(useProxy = false) {
+  let bot;
+  if (useProxy) {
+    const socksAgent = new SocksAgent({
+      socksHost: process.env.PROXY_HOST,
+      socksPort: process.env.PROXY_PORT,
+      socksUsername: process.env.PROXY_USER,
+      socksPassword: process.env.PROXY_PASSWORD,
+    });
 
-  const bot = new Telegraf(process.env.API_TOKEN, {
-    telegram: { agent: socksAgent },
-  });
+    bot = new Telegraf(process.env.API_TOKEN, {
+      telegram: { agent: socksAgent },
+    });
+  } else {
+    bot = new Telegraf(process.env.API_TOKEN);
+  }
 
   return bot;
 }
